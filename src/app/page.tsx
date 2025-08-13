5fb2de94cb6b4707173159309d7e635107d1d341
yuyamh <yuya.htr0828@gmail.com>
2025-08-13 18:56:37 +0900

    Hello UniverseをHello Galaxyに変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const Page: FC = () => {
     <main>
       <div className="flex justify-center items-center h-screen">
         <div className="flex flex-col items-center">
-          <h1 className="text-2xl font-bold">Hello Universe</h1>
+          <h1 className="text-2xl font-bold">Hello Galaxy</h1>
         </div>
       </div>
     </main>
